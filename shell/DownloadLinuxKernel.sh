53325902553e0ee2493d80bf7f885b9b055378fb
rechie <502117806@qq.com>
2016-12-06 15:17:31 +0800

    修改

--- shell/DownloadLinuxKernel.sh
+++ shell/DownloadLinuxKernel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # author:rechie
+# 用于实现linux-kernel的下载
 
 mkdir linux-stable
 cd linux-stable/
 git init
-git fetch git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-
+git remote add origin git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+git fetch
+git branch -r
