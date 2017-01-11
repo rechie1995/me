3f2361088827c4b125d8cbc3da3605d98343b5cd
rechie <502117806@qq.com>
2017-01-11 14:35:55 +0800

    add 腐蚀与膨胀

--- python/opencv/erode_dilate.py
+++ python/opencv/erode_dilate.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 # @author: rechie
+
+from cv2 import *
+
+def erode_dilate():
+    img = imread("/home/rechie/Pictures/6.jpg")
+    
+    # opencv定义的结构元素
+    kernel = getStructuringElement(MORPH_RECT,(3,3))
+    
+    # 腐蚀图像
+    eroded = erode(img, kernel)
+    # 显示腐蚀后的图像
+    imshow("Eroded Image", eroded)
+    
+    # 膨胀图像
+    dilated = dilate(img, kernel)
+    # 显示膨胀后的图像
+    imshow("Dilated Image", dilated)
+    
+    # 原图像
+    imshow("Origin", img)
+    
+    waitKey(0)
+    destroyAllWindows()
+
+if __name__ == '__main__':
+    erode_dilate()
