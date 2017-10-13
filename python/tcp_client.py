cd545e83f51316a6a5019b042f0eb9f93850ec28
rechie <502117806@qq.com>
2017-10-13 16:25:55 +0800

    修改tcp_client的bug

--- python/tcp_client.py
+++ python/tcp_client.py
@@ -12,10 +12,10 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 连接客户端 
-client = connect((target_host, target_port))
+client.connect((target_host, target_port))
 
 # 发送一些数据
-client = send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
+client.send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
 
 # 接收一些数据
 response = client.recv(4096)
